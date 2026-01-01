8dde5f4b769cea40cd33b12a46496667802c5cac
Adam Spanel <adam.spanel@seznam.cz>
2026-01-01 23:43:32 +0100

    try update csproj to include txt files

--- backend/obj/Debug/net10.0/OddOneOut.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/OddOneOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OddOneOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f48bde23a731c000011c7bd0cebf4e18911730b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81f36af08c1cb439a936a4ada9e0d7b7526c17c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OddOneOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OddOneOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
