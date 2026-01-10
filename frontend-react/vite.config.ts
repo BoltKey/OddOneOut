9e8d47258b984a11d48b6322ede880a3156a8547
Adam Spanel <adam.spanel@seznam.cz>
2026-01-10 17:50:34 +0100

    update to support pwa

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Misfit",
+        short_name: "Misfit",
+        description:
+          "A word game about guessing if your word is Match or Misfit.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "misfiticon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "misfiticon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
